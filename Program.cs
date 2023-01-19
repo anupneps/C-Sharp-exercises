2852a285f7ae4574d7cc56b5d446539a3f763743
anupneps <anup.neps@outlook.com>
2023-01-19 22:08:51 +0200

    challenge 2 done

--- Program.cs
+++ Program.cs
@@ -11,14 +11,37 @@ namespace CSharpFundamental
             string text = "this is a text";
             string newText = toTitleCase(text);
             Console.WriteLine(newText); // expect to see "This Is A Text"
+           
+            /* Challenge 2 */
+            int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
+            int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
+            int[,] result = matrixMultiply(arrayA, arrayB);
+            Console.WriteLine((result));//share your findings to Slack
             Console.ReadLine();
-
-            ///* Challenge 2 */
-            //int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
-            //int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
-            //int[,] result = matrixMultiply(arrayA, arrayB);
-            //Console.WriteLine(result); //share your findings to Slack
         }
+
+        static int[,] matrixMultiply(int[,] arrayA, int[,] arrayB)
+        {
+            if (arrayA.GetLength(0) != arrayB.GetLength(1))
+            {
+                throw new InvalidOperationException("Two arrays can't be multiplied");
+            }
+            int[,] result = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+            for (int i = 0; i < arrayA.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayB.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arrayA.GetLength(1); k++)
+                    {
+                        result[i, j] += arrayA[i, k] * arrayB[k, j];
+                    }
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return result;
+        }
+
         static string toTitleCase(string input)
         {
             string[] words = input.Split(' ');
@@ -29,9 +52,5 @@ namespace CSharpFundamental
             }
                      return string.Join(" ", wordList);
         }
-        //static int[,] matrixMultiply(int[,] array1, int[,] array2)
-        //{
-        //    /* Write your code here */
-        //}
     }
 }
